e686001e8e425fe2027ffaf17f5f2bc0297667b8
liming.xie <liming.xie@gmail.com>
2019-08-10 21:58:54 +0800

    增加基于glib的主事件循环.

--- Makefile
+++ Makefile
@@ -1,16 +1,22 @@
 
+APPNAME := BinApp
+
+CFLAGS := -Wall
+CFLAGS += -I /usr/include/glib-2.0
+CFLAGS += -L .
 
 OBJS := main.o service_idle.o  service_master.o  service_sys.o
 
+
 all: ${OBJS}
-	gcc -Wall -o binapp -L . ${OBJS} -lglib-2.0 -lpthread 
+	gcc  ${CFLAGS} -o ${APPNAME} ${OBJS} -lglib-2.0 -lpthread 
 
 
 %.o:%.c
-	gcc -o $@ -c $<
+	gcc ${CFLAGS} -o $@ -c $<
 
 
 clean:
 	@rm -f *.o *.a *.so *.bak
-	@rm -f binapp
+	@rm -f ${APPNAME}
 
--- main.c
+++ main.c
@@ -16,6 +16,12 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include <stdio.h>
+#include "glib.h"
+
+#include "service_idle.h"
+#include "service_master.h"
+#include "service_sys.h"
+
 
 #ifdef  __cplusplus
 extern "C" {
@@ -28,9 +34,19 @@ extern "C" {
 /* Private functions ---------------------------------------------------------*/
 
 
+/* 首先在main主程序中实现事件循环 */
+void main_event_loop(void)
+{
+	GMainLoop *loop;
+	loop = g_main_loop_new (NULL, FALSE);
+	g_main_loop_run (loop);
+	g_main_loop_unref (loop);
+}
+
 int main(int argc,char *argv[])
 {
-	printf("hello world!!!\n");
+	printf("hello BinApp!!!\n");
+	main_event_loop();
 	return 0;
 }
 
